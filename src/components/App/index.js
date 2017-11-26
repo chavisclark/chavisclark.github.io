69c8d06ee8b258f3319c9a0a6261a2cfd4922a59
Chā <chavis.clark@gmail.com>
2017-11-26 12:41:34 +0100

    new meta

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -72,9 +72,9 @@ function App({ children, routes }) {
         <div className={styles.banner}>
           <A href="/"><Img src={Profile} className={styles.logo} alt="chae profile photo" /></A>
           <div className={`${styles.logoText} ${styles.highlight}`}>
-            <span className={styles.h}>Creative Web Developer</span>
+            <span className={styles.h}>Nomadic Remote Web Developer</span>
+            <span className={styles.h}>Digital Creator of Things</span>
             <span className={styles.h}>Lifelong Learner</span>
-            <span className={styles.h}>Digital Nomad</span>
           </div>
         </div>
         <div className={styles.contact}>
