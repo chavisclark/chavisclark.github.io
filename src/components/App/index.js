07bb308e1a973964cbcabd3db91bac432457d521
Chā <chavis.clark@gmail.com>
2017-11-27 18:22:13 +0100

    mobile nav

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import Interactive from 'react-interactive';
 import { Link } from 'react-router';
 import Helmet from 'react-helmet';
@@ -19,21 +19,32 @@ const propTypes = {
   routes: PropTypes.array.isRequired,
 };
 
-function App({ children, routes }) {
-  const divImage = {
-    backgroundImage: `url('${Banner}')`,
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-  };
-  const divUpDownImage = {
-    backgroundImage: `url('${BannerUpDown}')`,
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-  };
-  function currentPath() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mobileOpen: false
+    }
+  }
+  handleOnClick() {
+    const nav = document.getElementsByTagName('nav')[0];
+    const trigger = document.getElementsByTagName('dd')[0];
+    if (!this.state.mobileOpen) {
+      this.setState({mobileOpen: true})
+      nav.style.display = 'flex';
+      trigger.innerHTML = '<i class="fa fa-2x fa-close"></i>';
+    } else {
+      this.setState({mobileOpen: false})
+      nav.style.display = 'none';
+      trigger.innerHTML = '<i class="fa fa-2x fa-bars"></i>';
+    }
+    
+    
+  }
+  currentPath() {
     return routes[1].path;
   }
-  function generateMapMenu() {
+  generateMapMenu() {
     let path = '';
 
     function nextPath(route) {
@@ -58,65 +69,77 @@ function App({ children, routes }) {
         ))
     );
   }
-
-  return (
-    <div className={styles.container}>
-      <Helmet
-        titleTemplate="%s"
-        defaultTitle="Chavis Clark"
-        meta={[
-          { name: 'description', content: 'About Chavis Clark ' },
-        ]}
-      />
-      <div style={divImage} className={styles.logoWrapper}>
-        <div className={styles.banner}>
-          <A href="/"><Img src={Profile} className={styles.logo} alt="chae profile photo" /></A>
-          <div className={`${styles.logoText} ${styles.highlight}`}>
-            <span className={styles.h}>Remote Fullstack Web Developer</span>
-            <span className={styles.h}>Digital Creator of Things</span>
-            <span className={styles.h}>Lifelong Learner</span>
+  render() {
+    const divImage = {
+      backgroundImage: `url('${Banner}')`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+    };
+    const divUpDownImage = {
+      backgroundImage: `url('${BannerUpDown}')`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+    };
+    return (
+      <div className={styles.container}>
+        <Helmet
+          titleTemplate="%s"
+          defaultTitle="Chavis Clark"
+          meta={[
+            { name: 'description', content: 'About Chavis Clark ' },
+          ]}
+        />
+        <div style={divImage} className={styles.logoWrapper}>
+          <div className={styles.banner}>
+            <A href="/"><Img src={Profile} className={styles.logo} alt="chae profile photo" /></A>
+            <div className={`${styles.logoText} ${styles.highlight}`}>
+              <span className={styles.h}>Remote Fullstack Web Developer</span>
+              <span className={styles.h}>Digital Creator of Things</span>
+              <span className={styles.h}>Lifelong Learner</span>
+            </div>
+          </div>
+          <div className={styles.contact}>
+            <A target="_blank" href="https://www.linkedin.com/in/1chac">
+              <figure className={styles.circle}>    
+                <i className={`${styles.icon} fa fa-linkedin fa-2x`}></i>
+              </figure>
+            </A>
+            <A target="_blank" href="https://twitter.com/cha_skyes">
+              <figure className={styles.circle}>
+                <i className={`${styles.icon} fa fa-twitter fa-2x`}></i>
+              </figure>
+            </A>
+            <A target="_blank" href="https://github.com/chavisclark">
+              <figure className={styles.circle}>
+                <i className={`${styles.icon} fa fa-github fa-2x`}></i>
+              </figure>
+            </A>
+            <A target="_blank" href="mailto:chavis.clark@gmail.com">
+              <figure className={styles.circle}>
+                <i className={`${styles.icon} fa fa-envelope fa-2x`}></i>
+              </figure>
+            </A>
           </div>
         </div>
-        <div className={styles.contact}>
-          <figure className={styles.circle}>
-            <A target="_blank" href="https://www.linkedin.com/in/1chac">
-              <i className={`${styles.icon} fa fa-linkedin fa-2x`}></i>
-            </A>
-          </figure>
-          <figure className={styles.circle}>
-            <A target="_blank" href="https://twitter.com/cha_skyes">
-              <i className={`${styles.icon} fa fa-twitter fa-2x`}></i>
-            </A>
-          </figure>
-          <figure className={styles.circle}>
-            <A target="_blank" href="https://github.com/chavisclark">
-              <i className={`${styles.icon} fa fa-github fa-2x`}></i>
-            </A>
-          </figure>
-          <figure className={styles.circle}>
-            <A target="_blank" href="mailto:chavis.clark@gmail.com">
-              <i className={`${styles.icon} fa fa-envelope fa-2x`}></i>
-            </A>
-          </figure>
+        <div style={divUpDownImage} className={styles.motto}>
+          <section className={`${styles.textSection} ${styles.centered}`}>
+            <p className={styles.body}>Solving problems works best <br />when you're standing on your head...</p>
+          </section>
         </div>
+        <dd onClick={this.handleOnClick.bind(this)} className={styles.mobileTrigger}><i className="fa fa-bars fa-2x"></i></dd>
+        <nav className={styles.navBar}>
+          <Button className={this.currentPath.bind(this) == '' ? 'active' : ''} location='/'>Home</Button>
+          <Button className={this.currentPath.bind(this) == 'cv' ? 'active' : ''} location='/cv'>Resume/CV</Button>
+          <Button className={this.currentPath.bind(this) == 'story' ? 'active' : ''} location='/story'>Read My Story</Button>
+          <Button className={this.currentPath.bind(this) == 'github-pages' ? 'active' : ''} location='/github-pages'>Github Projects</Button>
+        </nav>
+        <div className={this.currentPath.bind(this) == 'github-pages' ? '' : styles.wrapper}>
+          {this.props.children}
+        </div>
+        <Footer />
       </div>
-      <div style={divUpDownImage} className={styles.motto}>
-        <section className={`${styles.textSection} ${styles.centered}`}>
-          <p className={styles.body}>Solving problems works best <br />when you're standing on your head...</p>
-        </section>
-      </div>
-      <nav className={styles.navBar}>
-        <Button className={currentPath() == '' ? 'active' : ''} location='/'>Home</Button>
-        <Button className={currentPath() == 'cv' ? 'active' : ''} location='/cv'>Resume/CV</Button>
-        <Button className={currentPath() == 'story' ? 'active' : ''} location='/story'>Read My Story</Button>
-        <Button className={currentPath() == 'github-pages' ? 'active' : ''} location='/github-pages'>Github Projects</Button>
-      </nav>
-      <div className={currentPath() == 'github-pages' ? '' : styles.wrapper}>
-        {children}
-      </div>
-      <Footer />
-    </div>
-  );
+    );    
+  }
 }
 
 App.propTypes = propTypes;
